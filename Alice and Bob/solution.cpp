7b4304be3d7f71044c28be8892d0951d8cff9ac6
Anurag-6799 <singhanurag6799@gmail.com>
2026-01-28 23:41:47 +0530

    correcetd solution of Alice and Bob

---
+++ Alice and Bob/solution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc=1;
+    cin>>tc;
+    while(tc--){
+        int n, alice;
+        cin>>n>>alice;
+        vector<int>val(n);
+        int cnt=0;
+        for (auto &i : val)
+        {
+            cin >> i;
+            if (i > alice)
+                cnt++;
+            else if (i < alice)
+                cnt--;
+        }
+
+        cout << alice - 1 + 2 * (cnt > 0) << endl;
+    }
+}
+
